61ddd19b1cde8d4f6f7a83e0c3fa0fd60db09044
Iwan Haryatno <iwnharry61@gmail.com>
2025-09-13 07:41:06 +0700

    update player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,4 +6,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * 400
+	
 	move_and_slide()
