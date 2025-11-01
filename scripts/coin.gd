d60f9435e6d37fb6ff410c230969c2beff644a78
Iwan Haryatno <iwnharry61@gmail.com>
2025-11-01 07:16:48 +0700

    update: pertemuan terbaru

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -5,3 +5,6 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	queue_free()
+	Main.total_score += 1
+	
+	print("Total Coin:", Main.total_score)
--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+const SPEED = 60
+var direction = 1
+
+func _process(delta: float) -> void:
+	if $MataKanan.is_colliding():
+		direction = -1
+		$AnimatedSprite2D.flip_h = true
+	elif $MataKiri.is_colliding():
+		direction = 1
+		$AnimatedSprite2D.flip_h = false
+	
+	position.x += direction * SPEED * delta
