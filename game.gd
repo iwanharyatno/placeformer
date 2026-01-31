da294485387d3527f024ed0ca65f4f6a803635c6
Iwan Haryatno <iwnharry61@gmail.com>
2026-01-31 07:00:28 +0700

    basic scene navigation setup

--- game.gd
+++ game.gd
@@ -1,2 +1,21 @@
 class_name GameController
 extends Node2D
+
+var world: Node2D
+var gui: CanvasLayer
+
+var current_world: Node2D
+var current_gui: CanvasLayer
+
+func _ready() -> void:
+	world = $World
+	gui = $GUI
+	
+	current_world = $World/Home
+	current_gui = $GUI/Home
+
+func change_screen(path):
+	pass
+	
+func change_world(path):
+	pass
