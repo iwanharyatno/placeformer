49c4b56e851565d76f40adcf4f02a0b6ff597c97
Iwan Haryatno <iwnharry61@gmail.com>
2026-02-07 07:26:51 +0700

    pertemuan ke 3

--- game.gd
+++ game.gd
@@ -8,6 +8,8 @@ var current_world: Node2D
 var current_gui: CanvasLayer
 
 func _ready() -> void:
+	Main.gameController = self
+	
 	world = $World
 	gui = $GUI
 	
@@ -15,7 +17,15 @@ func _ready() -> void:
 	current_gui = $GUI/Home
 
 func change_screen(path):
-	pass
+	if current_gui:
+		gui.remove_child(current_gui)
+	var newScene = load(path).instantiate()
+	gui.add_child(newScene)
+	current_gui = newScene
 	
 func change_world(path):
-	pass
+	if current_world:
+		world.remove_child(current_world)
+	var newWorld = load(path).instantiate()
+	world.add_child(newWorld)
+	current_world = newWorld
---
+++ screens/home.gd
@@ -0,0 +1,5 @@
+extends CanvasLayer
+
+func _on_button_pressed() -> void:
+	Main.gameController.change_screen("res://screens/game_play.tscn")
+	Main.gameController.change_world("res://worlds/levels/first_level.tscn")
